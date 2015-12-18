069bc81aa17b42cd1f83b69534387f3afc222cd1
Alex Zolotko <azolotko@gmail.com>
2015-12-18 18:07:10 +0100

    Some responses don't have trace_id

--- lib/cassandra/cluster/client.rb
+++ lib/cassandra/cluster/client.rb
@@ -754,7 +754,8 @@ module Cassandra
                   request.consistency = decision.consistency
                   do_send_request_by_plan(host, connection, promise, keyspace, statement, options, request, plan, timeout, errors, hosts, retries + 1)
                 when Retry::Decisions::Ignore
-                  promise.fulfill(Results::Void.new(r.trace_id, keyspace, statement, options, hosts, request.consistency, retries, self, @futures))
+                  trace_id = r.respond_to?(:trace_id) ? r.trace_id : nil
+                  promise.fulfill(Results::Void.new(trace_id, keyspace, statement, options, hosts, request.consistency, retries, self, @futures))
                 when Retry::Decisions::Reraise
                   promise.break(r.to_error(statement))
                 else
